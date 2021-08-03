a76ca489e4f35d8ebaa4a8aa074b47cf01d072c9
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2021-08-02 22:52:46 -0500

    feat: relay application

--- src/src/main/java/com/home/crushedmonkey/relayApplication.java
+++ src/src/main/java/com/home/crushedmonkey/relayApplication.java
@@ -1,5 +1,11 @@
 package com.home.ManyToManyManyToOneAndOneToOne;
 
+
+
+//
+// Haham im love with this repo name
+//
+//
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class relayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CrushedMonkeyApplication.class, args);
+        SpringApplication.run(relayApplication.class, args);
     }
 
 }
